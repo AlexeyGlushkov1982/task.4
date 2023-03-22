e54bab0ed9f6814a022933f70cc1e9268b762739
glushkov <klepa_82@bk.ru>
2023-03-22 08:46:17 +0300

    add task.3

---
+++ task.3/Program.cs
@@ -0,0 +1,20 @@
+﻿int ReadInt(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] proba(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max);
+    }
+    return result;
+}
+int a = ReadInt("ВВедите размер массива - > ");
+int b = ReadInt("ВВедите min массива - > ");
+int c = ReadInt("ВВедите max массива - > ");
+int[] array = proba(a, b, c);
+Console.Write($"[{String.Join(",", array)}]");
